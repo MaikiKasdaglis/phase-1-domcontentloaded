b553ae6c0d9c0bb1b222b4011a2b790044f26c4b
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-06-14 16:14:33 -0600

    finishes lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", change
+    // labStuff = document.querySelector("#text")
+    // labStuff.textContent = "This is really cool!"
+    // console.log("This is really cool!");
+  );
+  console.log("This happens first")
+  function change(){
+    document.querySelector("#text").textContent = "This is really cool!"
+  }
+  
